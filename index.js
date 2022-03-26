f2e418a0188bea4960c0654ab907a97f5df38c03
louis <louis@systemli.org>
2022-03-26 23:28:31 +0100

    :sparkles: Make Plugin compatible with PostCSS 8

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const postcss = require('postcss')
-
-function plugin () {
-  return function (css) {
-    css.walkAtRules('import', rule => {
-      if (rule.params.includes('fonts.googleapis.com')) {
-        rule.remove()
-      }
-    })
+/**
+ * @type {import('postcss').PluginCreator}
+ */
+module.exports = () => {
+  return {
+    postcssPlugin: 'remove-google-fonts',
+    Root (root) {
+      root.walkAtRules('import', rule => {
+        if (rule.params.includes('fonts.googleapis.com')) {
+          rule.remove()
+        }
+      })
+    }
   }
 }
 
-module.exports = postcss.plugin('remove-google-fonts', plugin)
+module.exports.postcss = true
